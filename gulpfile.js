b36d6639dae5e820cc866d70c0858d62cf978544
Andrew Reynolds <areynold@illinois.edu>
2019-11-04 11:13:11 -0600

    replace jpegtran with mozjpeg

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var minimist = require('minimist');
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var imagemin = require('gulp-imagemin');
+var mozjpeg = require('imagemin-mozjpeg');
 
 /**
  * Set task defaults.
@@ -20,13 +21,20 @@ var options = minimist(process.argv.slice(2), defaults);
  * instead of requiring separate input and output directories.
  */
 gulp.task('minify-images', function () {
+    const plugins = [
+        imagemin.gifsicle(),
+        mozjpeg(),
+        imagemin.optipng(),
+        imagemin.svgo()
+    ];
+
     const workingdir = options.imageDir ?
         options.imageDir :
         defaults.imageDir;
     
     return gulp.src(workingdir)
         .pipe(plumber())
-        .pipe(imagemin())
+        .pipe(imagemin(plugins))
         .pipe(gulp.dest(function (file) {
             return file.base;
         }));
